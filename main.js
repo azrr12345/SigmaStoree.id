1e696f5d62eabf3fbeeb1c0419815835c3a93cd8
azrr12345 <alwi3023d@gmail.com>
2025-04-19 12:29:20 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+main.jslet nama = 'azril' // tipe staring
+let usia = 16 //tipe integer number
+let tinggiBadan = 166.6 // tipe double float 
+let beratBadan = 60
+
+let tabungan = 50000
+let uangJajan = 30000
+const kebutuhan = 40000
+const uangAkhir = tabungan + uangJajan - kebutuhan
+
+
+alert   (`pencet logo jika mau kembali keberanda`)
+
+alert    (`thxx dah buka`)
+alert   (`yg baca kek taii`)
+alert    (`itam bauuuu `)
